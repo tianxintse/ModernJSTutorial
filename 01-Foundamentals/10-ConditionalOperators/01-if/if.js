0031139ac43637f8ed8d05d235d0b2dbfb4fe20b
tianxintse <xietx1995@gmail.com>
2018-09-28 17:04:17 +0800

    study foundamentals

---
+++ 01-Foundamentals/10-ConditionalOperators/01-if/if.js
@@ -0,0 +1,5 @@
+"use strict";
+
+let year = prompt("Which year?", "");
+if (Number(year) === 2018)
+    alert("You are right!");
---
+++ 01-Foundamentals/10-ConditionalOperators/02-question-mark/question-mark.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let age = prompt('age?', 18);
+
+let message = (age < 3) ? 'Hi, baby!' :
+    (age < 18) ? 'Hello!' :
+        (age < 100) ? 'Greetings!' :
+            'What an unusual age!';
+
+alert( message );
